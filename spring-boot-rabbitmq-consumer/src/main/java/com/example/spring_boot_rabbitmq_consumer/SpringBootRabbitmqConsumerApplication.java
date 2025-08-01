64e6079a666164c431371065b5d43a3bff8eba57
Sahiru22 <sahirubalasuriya222@gmail.com>
2025-08-01 22:43:24 +0530

    first commit

---
+++ spring-boot-rabbitmq-consumer/src/main/java/com/example/spring_boot_rabbitmq_consumer/SpringBootRabbitmqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_rabbitmq_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitmqConsumerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-rabbitmq-producer/src/main/java/com/example/spring_boot_rabbitmq_producer/SpringBootRabbitmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_rabbitmq_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitmqProducerApplication.class, args);
+	}
+
+}
